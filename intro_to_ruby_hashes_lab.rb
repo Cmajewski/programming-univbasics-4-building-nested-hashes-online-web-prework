d0ce558fcb3a6dea755d9ea756cf81ff77fbc389
peaceful-cyborg-2337 <cmajewski@wesleyan.edu>
2019-07-15 03:51:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,12 @@
 def monopoly
-{:railroads=>{}}
+{
+	:railroads=>{}
+}
 end
 def monopoly_with_second_tier
-	{:railroads=>{pieces:4,names:{},rent_in_dollars:{}}
+	{
+		:railroads=>{pieces:4,names:{},rent_in_dollars:{}}
+	}
 end
 def monopoly_with_third_tier
 		{:railroads=>{pieces:4,names:{reading_railroad:{},pennsylvania_railroad:{},b_and_o_railroad:{},shortline:{}},rent_in_dollars:{one_piece_owned:25,two_pieces_owned:50,three_pieces_owned:200}}#c
